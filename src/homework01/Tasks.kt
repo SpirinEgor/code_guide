df31a199744b4aa6e1a83563072529e30c1fc85f
Kirill <kirill_gla@mail.ru>
2018-03-28 08:02:30 +0300

    - improved code quality

--- src/homework01/Tasks.kt
+++ src/homework01/Tasks.kt
@@ -1,28 +1,9 @@
 package homework01
 
-/**
- * -1 is added when value not found
- * to avoid ambiguous results when value
- * could've been inserted to 0th index
- *
- * @see binarySearchRecursion
- */
-fun linearSearch(array: List<Int>, element: Int, start: Int, end: Int, compare: (Int, Int) -> Int): Int {
-    for (i in start..end) {
-        if (compare(array[i], element) == 0) {
-            return i
-        }
-        if (compare(array[i], element) > 0) {
-            return -i - 1
-        }
-    }
-    return -end - 1
-}
+const val StartLinear = 5
 
 /**
- * start and end are assumed to be in [0..array.size]
- * also assumed that start <= end
- * array is assumed to be sorted in ascending order according to comparator
+ * Performs search in [start; end)
  *
  * @param compare thr comparator to be used.
  *        Should return negative value if first argument is smaller,
@@ -31,67 +12,46 @@ fun linearSearch(array: List<Int>, element: Int, start: Int, end: Int, compare: 
  *
  * @returns index of first occurrence of element
  */
-fun binarySearchRecursion(array: List<Int>, element: Int, start: Int, end: Int, compare: (Int, Int) -> Int): Int {
+private tailrec fun binarySearchRecursion(array: List<Int>, element: Int, start: Int, end: Int, compare: (Int, Int) -> Int): Int {
+    assert(start < end)
+    assert(start >= 0)
+    assert(end <= array.size)
 
-    // At some point recursive splitting is no longer that profitable
-    if (end - start < 5) {
-        return linearSearch(array, element, start, end, compare)
+    if (start == end - 1) {
+        return when {
+            compare(array[start], element) == 0 -> return start
+            compare(array[start], element) > 0 -> return start.inv()
+            else -> end.inv()
+        }
     }
 
-    val center = start + (end - start) / 2
+    val center = start + (end - start).shr(1)
 
-    // Case of element == array[center] is not taken into account
-    // due to possibility of duplicate elements
     return if (compare(element, array[center]) <= 0) {
         binarySearchRecursion(array, element, start, center, compare)
     } else {
-        binarySearchRecursion(array, element, center + 1, end, compare)
+        binarySearchRecursion(array, element, center, end, compare)
     }
 }
 
-/** Array is assumed to be sorted in ascending order
+/**
+ * Array is assumed to be sorted in ascending order
+ * Don't want to check that, it'd spoil asymptotic complexity
+ *
  * @returns index of first occurrence of element in array, if any
- *          otherwise (-1) * index of where it would be inserted to keep the array sorted
+ *          otherwise inversion index of where it would be inserted to keep the array sorted
+ *          (might therefore return array.size.inv())
  */
 fun binarySearch(array: List<Int>, f: Int): Int =
-        binarySearchRecursion(array, f, 0, array.size - 1, { first: Int, second: Int -> first - second })
+        binarySearchRecursion(array, f, 0, array.size, { first: Int, second: Int -> first - second })
 
 fun howManyNumbers(array: List<Int>, l: Int, r: Int): Int {
     val sorted = array.sorted()
-    val reversed = sorted.reversed()
-
-    val lFound = binarySearch(sorted, l)
-    val rFound = binarySearchRecursion(reversed, r, 0, reversed.size - 1, { first: Int, second: Int -> second - first })
-
-    // Now some weird magic here. Sorry
-    val first = if (lFound >= 0) lFound else -(lFound + 1)
-    val last = array.size - 1 + if (rFound >= 0) -rFound else rFound + 1
-
-    return last - first + 1
+    // [start; end)
+    val start = binarySearch(sorted, l)
+    val end = binarySearch(sorted, r + 1)
+    return (if (end >= 0) end else end.inv()) - if (start >= 0) start else start.inv()
 }
-
-/*
-I'm pretty sure it'd be more efficient this way, without any explicit sieve
-
-fun isPrime(number: Long, previousPrimes: List<Long>): Boolean = previousPrimes.all { number % it != 0L }
-
-fun getSumOfPrime(k: Int): Long {
-
-    val primes = ArrayList<Long>()
-
-    var currentNumber = 2L
-
-    while (primes.count() < k) {
-        if (isPrime(currentNumber, primes)) {
-            primes.add(currentNumber)
-        }
-        currentNumber++
-    }
-
-    // for k <= 0 primes will be empty and sum() will return 0
-    return primes.sum()
-}
-*/
 
 fun getSumOfPrime(k: Int): Long {
     val limit = 100000
@@ -115,65 +75,34 @@ fun getSumOfPrime(k: Int): Long {
     return -1
 }
 
-fun dumbCountTriples(a: List<Int>, b: List<Int>, c: List<Int>, x: Int): Int {
-    var sum = 0
-    for (aElement in a) {
-        for (bElement in b) {
-            for (cElement in c) {
-                if (aElement + bElement + cElement == x) {
-                    sum++
-                }
-            }
-        }
-    }
-    return sum
-}
-
-// I managed to get it in O(n^2log(n))
 fun countTriples(a: List<Int>, b: List<Int>, c: List<Int>, x: Int): Int {
-    val cSorted = c.sorted()
-    val cSortedReversed = cSorted.reversed()
+
+    val cMap = HashMap<Int, Int>()
+
+    for (cElement in c) {
+        cMap[cElement] = (cMap[cElement] ?: 0) + 1
+    }
 
     var sum = 0
 
     for (aElement in a) {
         for (bElement in b) {
-            val target = x - aElement - bElement
-            val cElementFound = binarySearch(cSorted, target)
-
-            if (cElementFound < 0) {
-                continue
-            }
-
-            val cElementEnd = c.size - 1 - binarySearchRecursion(
-                    cSortedReversed,
-                    target,
-                    0,
-                    c.size - 1,
-                    { first: Int, second: Int -> second - first }
-            )
-            val cElements = cElementEnd - cElementFound + 1
-            sum += cElements
+            sum += cMap[x - aElement - bElement] ?: 0
         }
     }
 
     return sum
 }
 
-fun findUnique(array: List<Int>): Int {
-    var result = 0
-    for (element in array) {
-        result = result xor element
-    }
-    return result
-}
+fun findUnique(array: List<Int>): Int =
+        array.fold(0, { a: Int, b: Int -> a xor b })
 
 /**
  * @returns 1 if values are ascending
  *          -1 if values are descending
  *          0 if index is a stationary point
  */
-fun getState(array: List<Int>, index: Int): Int =
+private fun getState(array: List<Int>, index: Int): Int =
         when (index) {
             0 -> when {
                 array[0] < array[1] -> 1
@@ -192,7 +121,7 @@ fun getState(array: List<Int>, index: Int): Int =
             }
         }
 
-fun linearSearchMax(array: List<Int>, start: Int, end: Int): Int {
+private fun linearSearchMax(array: List<Int>, start: Int, end: Int): Int {
     var indexOfMax = start
     for (i in start + 1..end) {
         if (array[i] > array[indexOfMax]) {
@@ -205,8 +134,8 @@ fun linearSearchMax(array: List<Int>, start: Int, end: Int): Int {
     return indexOfMax
 }
 
-fun ternarySearchRecursion(array: List<Int>, start: Int, end: Int): Int {
-    if (end - start < 5) {
+private tailrec fun ternarySearchRecursion(array: List<Int>, start: Int, end: Int): Int {
+    if (end - start < StartLinear) {
         linearSearchMax(array, start, end)
     }
 
