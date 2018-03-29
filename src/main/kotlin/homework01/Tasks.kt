7057f14bfe29aa06a743d106f36b3adf621ad7e9
voudy <spirin.egor@mail.ru>
2018-03-29 16:23:21 +0300

    add homework02

--- src/main/kotlin/homework01/Tasks.kt
+++ src/main/kotlin/homework01/Tasks.kt
@@ -1,5 +1,8 @@
 package homework01
 
+const val MAX_N = 1_000_000
+const val MAX_ITER = 1000
+
 fun binarySearch(array: List<Int>, f: Int): Int = 0
 
 fun howManyNumbers(array: List<Int>, l: Int, r: Int): Int = 0
---
+++ src/test/kotlin/homework01/TasksKtTest.kt
@@ -0,0 +1,32 @@
+package homework01
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class TasksKtTest {
+
+    @Test
+    fun binarySearch() {
+    }
+
+    @Test
+    fun howManyNumbers() {
+    }
+
+    @Test
+    fun getSumOfPrime() {
+    }
+
+    @Test
+    fun countTriples() {
+    }
+
+    @Test
+    fun findUnique() {
+    }
+
+    @Test
+    fun ternarySearch() {
+    }
+}
