5ba9e6a0a882f38dfe74e71b19887708c6385502
Kirill <kirill_gla@mail.ru>
2018-04-08 16:19:58 +0300

    - implemented bonus task

--- src/test/kotlin/homework03/TasksKtTest.kt
+++ src/test/kotlin/homework03/TasksKtTest.kt
@@ -1,6 +1,7 @@
 package homework03
 
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class TasksKtTest {
 
@@ -16,4 +17,43 @@ class TasksKtTest {
     fun backpack() {
     }
 
+    @Test
+    fun testContainsCorrectPairOnArrayContainingCorrectPair() {
+        val array = intArrayOf(1, 2, 3, 4, 7, 8, 9 ,10)
+
+        val result = containsCorrectPair(array.asList(), 7)
+        val correctResult = true
+
+        assertEquals(correctResult, result)
+    }
+
+    @Test
+    fun testContainsCorrectPairOnArrayNotContainingCorrectPair() {
+        val array = intArrayOf(1, 2, 3, 4, 7, 8, 9 ,10)
+
+        val result = containsCorrectPair(array.asList(), 42)
+        val correctResult = false
+
+        assertEquals(correctResult, result)
+    }
+
+    @Test
+    fun testContainsCorrectPairOnEmptyArray() {
+        val array = intArrayOf()
+
+        val result = containsCorrectPair(array.asList(), 42)
+        val correctResult = false
+
+        assertEquals(correctResult, result)
+    }
+
+    @Test
+    fun testContainsCorrectPairOnOneElementArray() {
+        val array = intArrayOf(1)
+
+        val result = containsCorrectPair(array.asList(), 42)
+        val correctResult = false
+
+        assertEquals(correctResult, result)
+    }
 }
