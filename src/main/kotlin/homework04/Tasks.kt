08c5490d57efc5cbf726e5a08fff36f62d4ae931
voudy <spirin.egor@mail.ru>
2018-04-13 14:11:45 +0300

    add homework04

---
+++ src/main/kotlin/homework04/Tasks.kt
@@ -0,0 +1,8 @@
+package homework04
+
+fun differentSubstrings(s: String): Int = 0
+
+fun manacker(s: String): Pair<List<Int>, List<Int>> = Pair(listOf(), listOf())
+
+fun autocompletion(words: List<String>): Int = 0
+
