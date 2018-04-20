73f744938b407938dddd2dbb56c45456c259ccf1
kartollika <maksimow.dmitrij@yandex.ru>
2018-04-20 20:38:20 +0300

    Implemented autocompletion function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
 import homework01.*
-import homework03.*
-import homework04.*
+import homework03.backpack
+import homework03.minimalDiffDivide
+import homework03.minimalScalarProduct
+import homework04.autocomplete
+import homework04.differentSubstrings
+import homework04.manacker
 import kotlin.reflect.KParameter
 import kotlin.reflect.jvm.javaMethod
 import kotlin.reflect.jvm.kotlinFunction
