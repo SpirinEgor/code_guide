bdfc937b048e77a9edc236b69b121c689ef7c57b
kartollika <maksimow.dmitrij@yandex.ru>
2018-04-24 22:09:39 +0300

    Implemented function to separate array into two arrays with minimum difference of sums of list's elements

--- src/main/kotlin/homework03/Tasks.kt
+++ src/main/kotlin/homework03/Tasks.kt
@@ -1,7 +1,42 @@
 package homework03
 
 
-fun minimalDiffDivide(array: List<Int>) = 0
+fun minimalDiffDivide(array: List<Int>): Int {
+    var sumInList1 = 0
+    var sumInList2 = 0
+
+    val sortedList = if (Math.abs(array.min()!!) >= Math.abs(array.max()!!)) {
+        array.sortedBy { it }
+    } else {
+        array.sortedByDescending { it }
+    }
+
+    for (i in 0 until sortedList.size) {
+        val addedElement = sortedList[i]
+
+        if (sumInList1 >= sumInList2) {
+            sumInList2 += addedElement
+        } else {
+            sumInList1 += addedElement
+        }
+    }
+
+    if (sumInList1 == 0)
+        return sumInList2
+
+    if (sumInList2 == 0)
+        return sumInList1
+
+    val diff1 = sumInList1 - sumInList2
+    val diff2 = sumInList2 - sumInList1
+
+
+    return if (diff1 <= diff2) {
+        diff1
+    } else {
+        diff2
+    }
+}
 
 fun minimalScalarProduct(x: List<Int>, y: List<Int>) = 0
 
