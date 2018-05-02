1354e87597d6d0c0ba572143f0e11e03a5030fbe
reutskaya <32382007+reutskaya@users.noreply.github.com>
2018-05-03 00:02:50 +0300

    homework03

--- src/main/kotlin/homework03/Tasks.kt
+++ src/main/kotlin/homework03/Tasks.kt
@@ -1,8 +1,43 @@
 package homework03
 
 
-fun minimalDiffDivide(array: List<Int>) = 0
+fun minimalDiffDivide(array: List<Int>): Int{
+    val sortedArray = array.sortedByDescending {it -> Math.abs(it)}
+    var sumList1 = 0
+    var sumList2 = 0
 
-fun minimalScalarProduct(x: List<Int>, y: List<Int>) = 0
+    for(i in 0 until sortedArray.size){
+        if(sortedArray[i] >= 0){
+            if(sumList1 >= sumList2)
+                sumList2 += sortedArray[i]
+            else
+                sumList1 += sortedArray[i]
+        }
+        else{
+            if(sumList1 >= sumList2)
+                sumList1 += sortedArray[i]
+            else
+                sumList2 += sortedArray[i]
+        }
+    }
 
-fun backpack(weight: List<Int>, cost: List<Int>, maxWeight: Int) = 0
+    return Math.abs(sumList1 - sumList2)
+}
+
+fun minimalScalarProduct(x: List<Int>, y: List<Int>): Int{
+    var scalarProduct = 0
+    val sortedX = x.sorted()
+    val sortedY = y.sortedDescending()
+
+    for (i in 0 until x.size){
+        scalarProduct += sortedX[i] * sortedY[i]
+    }
+
+    return scalarProduct
+}
+
+fun backpack(weight: List<Int>, cost: List<Int>, maxWeight: Int): Int{
+    //val backpack = Array(weight.size + 1, { Array(cost.size + 1, {0})})
+
+    return 0
+}
